87192439bd8b7ad73bd67c6a29064124829a950a
Terrence Chen <fcwestbbarca@126.com>
2023-12-26 19:07:05 +0800

    Commit1

---
+++ random_IP_address.py
@@ -0,0 +1,19 @@
+# Created by Terrence Chen
+# Greeting!
+
+
+# a =1
+# b=2
+# print(a+b)
+
+
+import random
+section1=random.randint(1,255)
+section2=random.randint(1,255)
+section3=random.randint(1,255)
+section4=random.randint(1,255)
+
+random.ip=str(section1)+'.'+str(section2)+'.'+str(section3)+'.'+str(section4)
+
+
+print(random.ip)
